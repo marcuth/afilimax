0772f2bfad943e6e74a492227cf6e26a01789d92
Marcuth <thiago.playgames15@gmail.com>
2025-07-15 03:12:10 -0300

    Add require-vanilla-puppeteer.ts

---
+++ src/utils/require-vanilla-puppeteer.ts
@@ -0,0 +1,13 @@
+export function requireVanillaPuppeteer() {
+    try {
+        return [require("puppeteer"), undefined]
+    }
+    catch (_) {}
+    
+    try {
+        return [require("puppeteer-core"), undefined]
+    }
+    catch (err) {
+        return [undefined, err]
+    }
+}
