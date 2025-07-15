628737212e71fb8c764b5069961dfab0b1f037e6
Marcuth <thiago.playgames15@gmail.com>
2025-07-15 03:12:08 -0300

    Add config.ts

---
+++ src/config.ts
@@ -0,0 +1,28 @@
+export const config = {
+    domains: {
+        amazon: ["amazon.com", "amazon.com.br", "amzn.to", "a.co"],
+        mercadoLivre: [
+            "mercadolibre.com.ar",
+            "mercadolibre.com.bo",
+            "mercadolivre.com.br",
+            "mercadolibre.cl",
+            "mercadolibre.com.co",
+            "mercadolibre.co.cr",
+            "mercadolibre.com.do",
+            "mercadolibre.com.ec",
+            "mercadolibre.com.gt",
+            "mercadolibre.com.hn",
+            "mercadolibre.com.mx",
+            "mercadolibre.com.ni",
+            "mercadolibre.com.pa",
+            "mercadolibre.com.py",
+            "mercadolibre.com.pe",
+            "mercadolibre.com.sv",
+            "mercadolibre.com.uy",
+            "mercadolibre.com.ve",
+            "mercadolivre.com"
+        ],
+        shopee: ["shopee.com.br", "shopee.com"],
+        aliExpress: ["aliexpress.com"],
+    }
+}
